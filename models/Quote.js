d11239931740c508ba2d539816e18a8f0c36971e
Vlad Trishch <vlad.trishch@gmail.com>
2018-03-23 22:34:18 +0200

    Created mongoose hook for resizing avatars on upload

--- models/Quote.js
+++ models/Quote.js
@@ -1,16 +1,23 @@
-import keystone from 'keystone';
-// var keystone = require('keystone')
-var Types = keystone.Field.Types
+import keystone from 'keystone'
+import gm from 'gm'
 
-var storage = new keystone.Storage({
+
+const { Types } = keystone.Field
+const im = gm.subClass({imageMagick: true})
+
+const storage = new keystone.Storage({
   adapter: keystone.Storage.Adapters.FS,
   fs: {
-    path: 'public/images/quoteAuthors/',
-    publicPath: '/public/images/quoteAuthors/',
+    path: 'public/images/quotesAuthors/',
+    publicPath: '/images/quotesAuthors/',
+  },
+  schema: {
+    path: true,
+    url: true,
   }
-});
+})
 
-var Quote = new keystone.List('Quote', {
+const Quote = new keystone.List('Quote', {
   map: {name: 'author'},
   singular: 'Quote',
   plural: 'Quotes',
@@ -19,9 +26,33 @@ var Quote = new keystone.List('Quote', {
 Quote.add({
   author: {type: String, required: true},
   text: {type: Types.Html, wysiwyg: true, height: 300},
-  authorAvatar: {type: Types.File, storage},
+  authorAvatar: {
+    // For some reason it can't be required in Schema naturally.
+    // For further details: https://github.com/keystonejs/keystone/issues/4575
+    type: Types.File,
+    storage,
+    note: 'Will be resized to 64x64',
+  },
 })
 
-Quote.defaultColumns = 'author, text'
+Quote.schema.pre('validate', function(next) {
+  const { path, filename } = this.authorAvatar
+
+  const url = path+filename
+  if(!url) {
+    // Either new Quote is being created or no image was specified,
+    // set the fallback one and omit it's resizing.
+    this.authorAvatar = {url: '/images/fallbacks/quotesAuthors.png'}
+    return next()
+  }
+  im(url)
+  .resizeExact(64, 64)
+  .write(url, next)
+})
+
+// TODO: Check for obsolete files and delete those.
+
+
+Quote.defaultColumns = 'author|20%, text'
 
 Quote.register()
