75de9bb9219f496dbbb8819989ae658898d8cd6f
Vlad Trishch <vlad.trishch@gmail.com>
2018-04-14 17:29:58 +0300

    One webpack config for front- and back- end

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,18 +1,23 @@
+import $ from 'jquery'
+import 'jquery-fatNav'
+import 'jquery-placeholder'
+import 'slick-carousel'
+
 window.onload = function () {
-  var $body = $('body')[0]
-  var $loader = $('.preloader')
+  var $body = $('body')[0];
+  var $loader = $('.preloader');
 
   if ($loader.size() > 0) {
-    setTimeout(function() {
-      $loader[0].style.opacity = 0
-      $loader[0].style.visibility = 'hidden'
-      $body.style.overflow = 'visible'
-    }, 1000)
+    setTimeout(function () {
+      $loader[0].style.opacity = 0;
+      $loader[0].style.visibility = 'hidden';
+      $body.style.overflow = 'visible';
+    }, 1000);
   }
-}
+};
 
-$(function() {
-  $('.quotes-slider__photo').hide()
+$(function () {
+  $('.quotes-slider__photo').hide();
   $('.quotes-slider').slick({
     autoplay: true,
     autoplaySpeed: 10000,
@@ -20,94 +25,89 @@ $(function() {
     pauseOnDotsHover: true,
     dots: true,
     dotsClass: 'quotes-slider__nav',
-    customPaging: function(slider, i) {
-      var $pic = slider.$slides.eq(i).find('.quotes-slider__photo')
+    customPaging: function (slider, i) {
+      var $pic = slider.$slides.eq(i).find('.quotes-slider__photo');
 
-      return $pic.clone().show()
+      return $pic.clone().show();
     }
-  })
+  });
 
   /*---------------------------------------------------*/
 
-  $('body').on('change', '.courses-filter select', function() {
+  $('body').on('change', '.courses-filter select', function () {
     var filterType = $(this).data('filter'),
-      filterVal = $(this).val(),
-      $filteredItems = $('.courses .preview')
+        filterVal = $(this).val(),
+        $filteredItems = $('.courses .preview');
 
-    if (!filterType) return 0
-    if(filterVal === 'all') return $filteredItems.show(300)
+    if (!filterType) return 0;
+    if (filterVal === 'all') return $filteredItems.show(300);
 
-    const filterFunc = filterType === 'age' ?
-      ((i, el) => $(el).data(filterType) <= filterVal) :
-      ((i, el) => $(el).data(filterType) == filterVal)
+    const filterFunc = filterType === 'age' ? (i, el) => $(el).data(filterType) <= filterVal : (i, el) => $(el).data(filterType) == filterVal;
 
-    const $itemsToShow = $filteredItems.filter(filterFunc)
-    const $itemsToHide = $filteredItems.filter((i, el) => !filterFunc(i, el))
+    const $itemsToShow = $filteredItems.filter(filterFunc);
+    const $itemsToHide = $filteredItems.filter((i, el) => !filterFunc(i, el));
 
-    $itemsToHide.hide(300)
-    $itemsToShow.show(300)
-
-  })
+    $itemsToHide.hide(300);
+    $itemsToShow.show(300);
+  });
 
   /*---------------------------------------------------*/
 
-  $('input[placeholder], textarea[placeholder]').placeholder()
+  $('input[placeholder], textarea[placeholder]').placeholder();
+});
 
-})
-
-$(document).ready(function() { // Course share buttons
+$(document).ready(function () {
+  // Course share buttons
   // Doesn't work for localhost at all.
-  $('.fb-share').attr('href', 'https://www.facebook.com/sharer/sharer.php?u='+window.location)
-  $('.twitter-share').attr('href', 'https://twitter.com/intent/tweet?url='+window.location)
-})
+  $('.fb-share').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + window.location);
+  $('.twitter-share').attr('href', 'https://twitter.com/intent/tweet?url=' + window.location);
+});
 
-$(document).ready(function() { // FAQ Sliding
-  var sliderSpeed = 300
+$(document).ready(function () {
+  // FAQ Sliding
+  var sliderSpeed = 300;
 
-  $('.faq-wrapper .item-wrapper').click(function() {
-    var $this = $(this)
-    var currItem = $this.attr('id').slice(-1)
-    if($this.hasClass('open')) {
-      $this.removeClass('open')
-      $('[id^=item-subtitle-'+currItem+']').slideUp(sliderSpeed).removeClass('open')
-      $('[id^=item-text-'+currItem+']').slideUp(sliderSpeed)
+  $('.faq-wrapper .item-wrapper').click(function () {
+    var $this = $(this);
+    var currItem = $this.attr('id').slice(-1);
+    if ($this.hasClass('open')) {
+      $this.removeClass('open');
+      $('[id^=item-subtitle-' + currItem + ']').slideUp(sliderSpeed).removeClass('open');
+      $('[id^=item-text-' + currItem + ']').slideUp(sliderSpeed);
+    } else {
+      $this.addClass('open');
+      $('[id^=item-subtitle-' + currItem + ']').slideDown(sliderSpeed);
     }
-    else {
-      $this.addClass('open')
-      $('[id^=item-subtitle-'+currItem+']').slideDown(sliderSpeed)
-    }
-  })
+  });
 
-  $('.faq-wrapper .subtitle-wrapper').click(function(event) {
-    event.stopPropagation()
-    var $this = $(this)
-    var currItem = $this.attr('id').slice(-3)
-    if($this.hasClass('open')) {
-      $this.removeClass('open')
-      $('#item-text-'+currItem).slideUp(sliderSpeed)
+  $('.faq-wrapper .subtitle-wrapper').click(function (event) {
+    event.stopPropagation();
+    var $this = $(this);
+    var currItem = $this.attr('id').slice(-3);
+    if ($this.hasClass('open')) {
+      $this.removeClass('open');
+      $('#item-text-' + currItem).slideUp(sliderSpeed);
+    } else {
+      $this.addClass('open');
+      $('#item-text-' + currItem).slideDown(sliderSpeed);
     }
-    else {
-      $this.addClass('open')
-      $('#item-text-'+currItem).slideDown(sliderSpeed)
-    }
-  })
+  });
 
-  $('.faq-wrapper .text-wrapper').click(function(event) {
-    event.stopPropagation()
-    var $this = $(this)
-    var currItem = $this.attr('id').slice(-3)
-    if($this.parent().find('.subtitle-wrapper').hasClass('open')) {
-      $this.parent().find('.subtitle-wrapper').removeClass('open')
-      $('#item-text-'+currItem).slideUp(sliderSpeed)
+  $('.faq-wrapper .text-wrapper').click(function (event) {
+    event.stopPropagation();
+    var $this = $(this);
+    var currItem = $this.attr('id').slice(-3);
+    if ($this.parent().find('.subtitle-wrapper').hasClass('open')) {
+      $this.parent().find('.subtitle-wrapper').removeClass('open');
+      $('#item-text-' + currItem).slideUp(sliderSpeed);
+    } else {
+      $this.parent().find('.subtitle-wrapper').addClass('open');
+      $('#item-text-' + currItem).slideDown(sliderSpeed);
     }
-    else {
-      $this.parent().find('.subtitle-wrapper').addClass('open')
-      $('#item-text-'+currItem).slideDown(sliderSpeed)
-    }
-  })
-})
+  });
+});
 
-$(document).ready(function() {
+$(document).ready(function () {
   $('.gallery-preview-slider').slick({
     autoplay: true,
     autoplaySpeed: 5000,
@@ -121,57 +121,54 @@ $(document).ready(function() {
         slidesToShow: 2,
         arrows: false
       }
-    }],
-  })
-  $('.swipebox').swipebox({ loopAtEnd: true })
+    }]
+  });
+  $('.swipebox').swipebox({ loopAtEnd: true });
   $('.slick-news-slider').slick({
     lazyLoad: 'progressive',
     draggable: false,
     infinite: true,
     prevArrow: '.slick-news-slider-prev',
-    nextArrow: '.slick-news-slider-next',
-  })
-})
+    nextArrow: '.slick-news-slider-next'
+  });
+});
 
+$.fn.filterData = function (key, value, equality) {
+  return this.filter(function () {
+    return equality ? $(this).data(key) == value : $(this).data(key) != value;
+  });
+};
 
-$.fn.filterData = function(key, value, equality) {
-  return this.filter(function() {
-    return equality ?
-      $(this).data(key) == value :
-      $(this).data(key) != value
-  })
-}
+$(document).ready(function () {
+  // Gallery filter
+  $('.filter-button').on('click', function () {
+    $('.filter-button.active').removeClass('active');
+    $(this).addClass('active');
 
-$(document).ready(function() { // Gallery filter
-  $('.filter-button').on('click', function() {
-    $('.filter-button.active').removeClass('active')
-    $(this).addClass('active')
+    const filterType = 'type';
+    const filterVal = $(this).data('filter');
+    const $filteredItems = $('.preview');
 
-    const filterType = 'type'
-    const filterVal = $(this).data('filter')
-    const $filteredItems = $('.preview')
+    if (!filterType) return 0;
+    if (filterVal === 'all') return $filteredItems.show(300).addClass('swipebox');
 
-    if (!filterType) return 0
-    if(filterVal === 'all') return $filteredItems.show(300).addClass('swipebox')
+    const filterFunc = (i, el) => $(el).data(filterType) == filterVal;
 
-    const filterFunc = (i, el) => $(el).data(filterType) == filterVal
+    const $itemsToShow = $filteredItems.filter(filterFunc);
+    const $itemsToHide = $filteredItems.filter((i, el) => !filterFunc(i, el));
 
-    const $itemsToShow = $filteredItems.filter(filterFunc)
-    const $itemsToHide = $filteredItems.filter((i, el) => !filterFunc(i, el))
+    $itemsToHide.hide(300).removeClass('swipebox');
+    $itemsToShow.show(300).addClass('swipebox');
+  });
+});
 
-    $itemsToHide.hide(300).removeClass('swipebox')
-    $itemsToShow.show(300).addClass('swipebox')
-
-  })
-})
-
-$(document).ready(function (){
-  $.fatNav()
-  const $scroll = () => $('html, body').animate({ scrollTop: $('.contacts-section').offset().top - $('.header').height() }, 1000)
-  $('.main-nav [data-navlink-key="contacts"]').click($scroll)
+$(document).ready(function () {
+  $.fatNav();
+  const $scroll = () => $('html, body').animate({ scrollTop: $('.contacts-section').offset().top - $('.header').height() }, 1000);
+  $('.main-nav [data-navlink-key="contacts"]').click($scroll);
   $('.fat-nav [data-navlink-key="contacts"]').click(() => {
-    $scroll()
-    $.fatNav().toggleNav()
-    $('.hamburger.active').removeClass('active')
-  })
-})
+    $scroll();
+    $.fatNav().toggleNav();
+    $('.hamburger.active').removeClass('active');
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,38 @@ require('dotenv').config()
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
-
-module.exports = {
-  context: __dirname,
-  target: 'node',
-  externals: [nodeExternals()],
-  entry: './keystone.js',
-  mode: process.env.NODE_ENV,
-
-  output: {
-    path: path.resolve(__dirname, './'),
-    filename: 'build.js'
-  },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      use: {
-        loader: 'babel-loader',
-        query: {
-          // Can't use .babelrc because some weird things happen to ./node_modules/keystone/admin/bundles/index.js
-          "plugins": [
-            ["transform-runtime", {
-              "polyfill": false,
-              "regenerator": true
-            }],
-            "transform-object-rest-spread"
-          ],
-          "presets": ["latest", "es2015", "stage-0"]
+module.exports = function(env) {
+  const frontend = env.side === 'frontend'
+  return {
+    context: path.resolve(__dirname),
+    target: frontend ? 'web' : 'node',
+    externals: frontend ? [] : [nodeExternals()],
+    entry: frontend ? './public/js/scripts.js' : './keystone.js',
+    output: {
+      path: path.resolve(__dirname, frontend ? './public/js/': ''),
+      filename: 'build.js'
+    },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            // Can't use .babelrc because some weird things happen to ./node_modules/keystone/admin/bundles/index.js
+            "plugins": [
+              ["transform-runtime", {
+                "polyfill": false,
+                "regenerator": true
+              }],
+              "transform-object-rest-spread"
+            ],
+            "presets": ["latest", "es2015", "stage-0"]
+          }
         }
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
+      }]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+  }
 }
